77fac3c0586ecf792df0c134e5066b12476133f1
Kingm4s <mdone3074@gmail.com>
2024-10-14 09:09:27 +0200

    examen

---
+++ appweb-main/web/public/javascripts/front.js
@@ -0,0 +1,7 @@
+document.querySelector("a#open-offcanvas").addEventListener("click",()=>{
+    document.querySelector("aside#offcanvas").classList.add("show");
+});
+
+document.querySelector("#offcanvas button.close").addEventListener("click",()=>{
+    document.querySelector("aside#offcanvas").classList.remove("show");
+});
